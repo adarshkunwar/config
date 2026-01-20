cf32e23eb213687a5a56a0a25681e57981507c40
Adarsh Kunwar <adarshkunwarofficial@gmail.com>
2026-01-20 22:17:11 +0545

    feat: clean up the options

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -22,11 +22,6 @@
 
 ]]
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.opt.clipboard = 'unnamedplus'
-vim.g.have_nerd_font = true
-
 require 'options'
 require 'keymaps'
 require 'lazy-bootstrap'
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,71 +1,70 @@
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+------------------------------------------
+--- Map leader and clipboard
+------------------------------------------
 
-vim.opt.title = true
-vim.opt.wildignore:append { '*/node_modules/*' }
-
--- Make line numbers default
-vim.opt.number = true
-vim.opt.relativenumber = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-
-vim.bo.tabstop = 2
-vim.bo.softtabstop = 2
-vim.bo.shiftwidth = 2
-vim.bo.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.opt.clipboard = 'unnamedplus'
 
--- Enable break indent
-vim.opt.breakindent = true
+------------------------------------------
+--- Base Options
+------------------------------------------
 
--- Save undo history
-vim.opt.undofile = true
+local opt = vim.opt
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+-- Title options
+opt.title = false
 
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+-- Line number
+opt.number = true
+opt.relativenumber = true
 
--- Decrease update time
-vim.opt.updatetime = 250
+-- Tab options & Indent
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+opt.breakindent = true
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+-- Don't show mode (handled by statusline)
+opt.showmode = false
 
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+-- Persistent undo
+opt.undofile = true
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
+-- Search behavior
+opt.ignorecase = true
+opt.smartcase = true
 
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+-- Always show the sign column
+opt.signcolumn = 'yes'
 
--- Show which line your cursor is on
-vim.opt.cursorline = true
+-- Faster time
+opt.updatetime = 250
+opt.timeoutlen = 300
 
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+-- Split behavior
+opt.splitright = true
+opt.splitbelow = true
 
---vim: ts=2 sts=2 sw=2 et
+-- Whitespace visualization
+opt.list = true
+opt.listchars = {
+  tab = '▸ ',
+  trail = '·',
+  nbsp = '␣',
+}
+
+-- Live substitution preview (Neovim ≥ 0.7)
+opt.inccommand = 'split'
+
+-- Highlight current line
+opt.cursorline = true
+
+-- Keep context around cursor
+opt.scrolloff = 10
+
+-- Ignore node_modules in file completion
+opt.wildignore:append '*/node_modules/*'
